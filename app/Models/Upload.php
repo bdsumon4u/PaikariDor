663fdf8fd88430d9d8580bbdf1c80f9e000c62ee
Sumon Ahmed <bdsumon4u@gmail.com>
2023-10-25 14:15:14 +0600

    UploadedOnindaAssets

--- app/Models/Upload.php
+++ app/Models/Upload.php
@@ -10,6 +10,13 @@ class Upload extends Model
     use SoftDeletes;
 
     /**
+     * The connection name for the model.
+     *
+     * @var string|null
+     */
+    protected $connection = 'oninda';
+
+    /**
     * The attributes that are mass assignable.
     *
     * @var array
--- config/database.php
+++ config/database.php
@@ -54,6 +54,21 @@ return [
             'engine' => null,
         ],
 
+        'oninda' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_ONINDA', 'forge'),
+            'username' => env('DB_ONINUSER', 'forge'),
+            'password' => env('DB_ONINPASS', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
