b545cc9d7ac3550c11ed610e79edfa8bca1374f9
Sumon Ahmed <bdsumon4u@gmail.com>
2023-10-25 13:18:38 +0600

    Hide: Products From SideNav

--- resources/views/backend/inc/admin_sidenav.blade.php
+++ resources/views/backend/inc/admin_sidenav.blade.php
@@ -59,7 +59,7 @@
 
                 <!-- Product -->
                 @canany(['add_new_product', 'show_all_products','show_in_house_products','show_seller_products','show_digital_products','product_bulk_import','product_bulk_export','view_product_categories', 'view_all_brands','view_product_attributes','view_colors','view_product_reviews'])
-                    <li class="aiz-side-nav-item">
+                    <li class="aiz-side-nav-item d-none">
                         <a href="#" class="aiz-side-nav-link">
                             <i class="las la-shopping-cart aiz-side-nav-icon"></i>
                             <span class="aiz-side-nav-text">{{translate('Products')}}</span>
